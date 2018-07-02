ac5a48f5b7d5dc4ebaf0f661751c92f5b44c8209
Ethan <ethan_chang@icloud.com>
2018-07-02 14:51:09 -0700

    fixed opencv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OpenCVtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OpenCVtest.java
@@ -3,24 +3,25 @@ package org.firstinspires.ftc.teamcode.Testing;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.RobotFunctions.visionStuff.CameraViewDisplay;
-import org.firstinspires.ftc.teamcode.RobotFunctions.visionStuff.OpenCV;
-import org.firstinspires.ftc.teamcode.RobotFunctions.visionStuff.TestDetector;
+import org.firstinspires.ftc.teamcode.RobotFunctions.FalconVision.CameraViewDisplay;
+import org.firstinspires.ftc.teamcode.RobotFunctions.FalconVision.OpenCVpipeline;
+import org.firstinspires.ftc.teamcode.RobotFunctions.FalconVision.TestDetector;
 
 @Autonomous
 public class OpenCVtest extends LinearOpMode{
-    OpenCV opencv;
+    OpenCVpipeline opencv;
 
     public void runOpMode(){
         opencv = new TestDetector();
         opencv.init(hardwareMap.appContext, CameraViewDisplay.getInstance(), 0);
 
         waitForStart();
+        opencv.enable();
 
         while(opModeIsActive()){
-            opencv.enable();
+
         }
 
-
+        opencv.disable();
     }
 }
