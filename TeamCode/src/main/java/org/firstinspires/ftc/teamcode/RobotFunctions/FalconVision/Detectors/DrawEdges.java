22551e0973f3a99891f457f0522c7037bc81a395
Ethan <ethan_chang@icloud.com>
2018-07-03 13:54:07 -0700

    tested values in contour detector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/DrawEdges.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/DrawEdges.java
@@ -7,14 +7,15 @@ import org.opencv.imgproc.Imgproc;
 
 public class DrawEdges extends OpenCVpipeline {
 
-    private Mat gray = new Mat();
+    private Mat grayScale = new Mat();
     private Mat blured = new Mat();
     private Mat edges = new Mat();
 
     public Mat processFrame(Mat rgba, Mat gray){
-        this.gray = gray;
-        Imgproc.GaussianBlur(gray, blured, new Size(5, 5), 0, 0);
-        Imgproc.Canny(blured, edges, 50, 150);
+
+        Imgproc.cvtColor(rgba, grayScale, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(grayScale, blured, new Size(5, 5), 0, 0);
+        Imgproc.Canny(blured, edges, 20, 150);
 
         return edges;
     }
