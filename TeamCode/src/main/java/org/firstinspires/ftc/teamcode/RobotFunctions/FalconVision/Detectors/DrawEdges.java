960bc4dc097e07cfa6c87760a4f256fd43b5b0fc
Ethan <ethan_chang@icloud.com>
2018-08-17 14:50:43 -0700

    drawing bounding boxes now works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/DrawEdges.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/DrawEdges.java
@@ -4,6 +4,7 @@ import org.firstinspires.ftc.teamcode.RobotFunctions.FalconVision.OpenCVpipeline
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -20,40 +21,43 @@ public class DrawEdges extends OpenCVpipeline {
     private Mat filteredLow = new Mat();
     private Mat filteredHigh = new Mat();
     private Mat filteredCombined = new Mat();
-    private Mat edges = new Mat();
     private List<MatOfPoint> contours = new ArrayList<>();
     private Scalar lowFilterHigh = new Scalar(15, 255, 255); //for two different filters, because the range for orange is around 0 and around 360 (hue)
-    private Scalar lowFilterLow = new Scalar(0, 20, 120);
+    private Scalar lowFilterLow = new Scalar(0, 100, 80);
     private Scalar highFilterHigh = new Scalar(180, 255, 255);
-    private Scalar highFilterLow = new Scalar(175, 20, 120);
-    private MatOfPoint pts = new MatOfPoint();
-    private Rect boundingRect = new Rect();
-    private int maxAreaID;
-    private double maxVal;
-    private double maxArea;
-    double contourArea;
+    private Scalar highFilterLow = new Scalar(175, 100, 80);
+    MatOfPoint2f approxCurve = new MatOfPoint2f();
+    double rectArea;
 
 
     public Mat processFrame(Mat rgba, Mat gray){
         this.rgba = rgba;
         Imgproc.cvtColor(rgba, hsv, Imgproc.COLOR_RGB2HSV, 3);
-        Imgproc.blur(hsv, blurred, new Size(15, 15));
+        Imgproc.blur(hsv, blurred, new Size(5, 5));
         Core.inRange(blurred, lowFilterLow, lowFilterHigh, filteredLow);
         Core.inRange(blurred, highFilterLow, highFilterHigh, filteredHigh);
         Core.add(filteredLow, filteredHigh, filteredCombined);
-        //Imgproc.findContours(filteredCombined, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(filteredCombined, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        Core.findNonZero(filteredCombined, pts);
+        Rect[] rect = new Rect[contours.size()];
 
-        boundingRect = Imgproc.boundingRect(pts);
-        Imgproc.rectangle(rgba, boundingRect.tl(), boundingRect.br(), new Scalar(0,0,255), 5);
+        for (int i = 0; i < contours.size(); i++) {
+            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray());
+            double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
+            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+            MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+            rect[i] = Imgproc.boundingRect(points);
+        }
 
+        for (int i = 0; i < contours.size(); i++){
 
-        //Imgproc.drawContours(rgba, contours, maxAreaID, new Scalar(255, 0, 0), 5);
-        maxVal = 0; //resets max contour area after finding largest contour
+            Imgproc.rectangle(rgba, rect[i].tl(), rect[i].br(), new Scalar(0, 255, 255), 5);
+        }
+
+        contours.clear();
 
         return rgba;
     }
 
-    public double getMaxArea(){return maxArea;}
+    public double getMaxArea(){return rectArea;}
 }
